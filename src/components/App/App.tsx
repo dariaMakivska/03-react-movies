a2732ebd21246171ec93a92e158928f738d0be8c
Daria Makivska <peony.bluet@gmail.com>
2025-05-26 00:07:39 +0300

    Додає movieService.ts та виправляє App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,15 @@
 import css from './App.module.css'
 import SearchBar from '../SearchBar/SearchBar';
 import { useState } from 'react'
+import fetchMovies from '../../services/movieService'
 import type { Movie } from "../../types/movie";
 import toast from 'react-hot-toast';
-import axios from 'axios';
+// import axios from 'axios';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
+
 
 
 export default function App() {
@@ -17,26 +19,14 @@ export default function App() {
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const myKey = import.meta.env.VITE_TMDB_TOKEN;
-  const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
   const handleSearch = async (query: string) => {
     setLoading(true);
     setError(false);
     setMovies([]);
-    try {
-      const response = await axios.get(BASE_URL, {
-        params: {
-          query,
-          include_adult: false,
-          language: "en-US",
-          page: 1,
-        },
-        headers: {
-          Authorization: `Bearer ${myKey}`
-        }
-      });
 
-      const results: Movie[] = response.data.results;
+    try {
+      const results = await fetchMovies(query, myKey); 
 
       if (results.length === 0) {
         toast.error("No movies found for your request.");
